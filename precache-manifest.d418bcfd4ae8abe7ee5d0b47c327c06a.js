ed919ef51a2cffd784d3c35b689b752ccf21ae69
Angelo Williams <awilliams21@wooster.edu>
2020-12-04 20:36:21 -0500

    Updates

---
+++ precache-manifest.d418bcfd4ae8abe7ee5d0b47c327c06a.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8283d0afacc19fa853b34bdd23bb07a",
+    "url": "/woomaps/index.html"
+  },
+  {
+    "revision": "5ea315314818d6d980b0",
+    "url": "/woomaps/static/css/main.d895f485.chunk.css"
+  },
+  {
+    "revision": "0ab807581c8b09cac26b",
+    "url": "/woomaps/static/js/2.a578f11d.chunk.js"
+  },
+  {
+    "revision": "5ea315314818d6d980b0",
+    "url": "/woomaps/static/js/main.0e66f1cd.chunk.js"
+  },
+  {
+    "revision": "02f822dd9a1dcc0e98c8",
+    "url": "/woomaps/static/js/runtime-main.d89b5d86.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/woomaps/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/woomaps/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/woomaps/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/woomaps/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/woomaps/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/woomaps/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/woomaps/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/woomaps/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "256d62da587eaf1baf3e0f657ac17b2f",
+    "url": "/woomaps/static/media/academic.256d62da.PNG"
+  },
+  {
+    "revision": "4d5e9bdd0fa5a964157d3446980fa94a",
+    "url": "/woomaps/static/media/backbutton.4d5e9bdd.PNG"
+  },
+  {
+    "revision": "650ff366c134f806c2a50ceed48b4f91",
+    "url": "/woomaps/static/media/dining.650ff366.PNG"
+  },
+  {
+    "revision": "5a75ce2a5c10b6133719596f90671ab1",
+    "url": "/woomaps/static/media/emptyspace.5a75ce2a.PNG"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/woomaps/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/woomaps/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/woomaps/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/woomaps/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/woomaps/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/woomaps/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/woomaps/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/woomaps/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/woomaps/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/woomaps/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/woomaps/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/woomaps/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/woomaps/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/woomaps/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/woomaps/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "a7f3e212136a53c26bb74bb5be6f809f",
+    "url": "/woomaps/static/media/linking.a7f3e212.PNG"
+  },
+  {
+    "revision": "42b486780a4cbaa8298f17b81966466c",
+    "url": "/woomaps/static/media/mode.42b48678.PNG"
+  },
+  {
+    "revision": "6031d131f1b5c30e66513de7786a1b62",
+    "url": "/woomaps/static/media/navigation.6031d131.PNG"
+  },
+  {
+    "revision": "0a65e912cd6862a3ed99e8c29456ccf5",
+    "url": "/woomaps/static/media/report.0a65e912.PNG"
+  },
+  {
+    "revision": "bdcabc258744649c9ff03baa62181142",
+    "url": "/woomaps/static/media/tkinter.bdcabc25.PNG"
+  },
+  {
+    "revision": "e878e64cbc19422313b6bb9b172170b0",
+    "url": "/woomaps/static/media/woomapsLogo.e878e64c.png"
+  }
+]);
